c5dc23a08cc4684bb7f5ad3b169b72f10cb20432
f1r3starter <andrey.filenochko@gmail.com>
2018-09-26 15:08:31 +0300

    lesson 4

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,22 +1,28 @@
 // Core
 import React, { Component } from 'react';
 
+// Components
+import { Consumer } from 'components/HOC/withProfile';
+
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
-    render() {
-        const { currentUserFirstName, avatar } = this.props;
+    render () {
         return (
-                <section className = { Styles.composer }>
-                    <img src = { avatar } />
-                    <form>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.composer } >
+                        <img src = { context.avatar } />
+                        <form>
                         <textarea
-                            placeholder = { `What's on your mind, ${currentUserFirstName}?` }
+                            placeholder = { `What's on your mind, ${context.currentUserFirstName}?` }
                         />
-                        <input type = 'submit' value = 'Post' />
-                    </form>
-                </section>
-        )
+                            <input type = 'submit' value = 'Post' />
+                        </form>
+                    </section>
+                )}
+            </Consumer>
+        );
     }
 }
